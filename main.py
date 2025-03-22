b10b8cd529f4cabf4d686add1ec590c8a44ee509
Tomas Svoboda <fredka171@gmail.com>
2025-03-22 22:42:40 +0100

    add TODO comments for future enhancements in main.py.

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 import window
+
+"""
+ToDo
+- vytvorit window v tkinteru s buttonem a menicim se labelem
+- vytvorit database s eng a czech slovicky
+- vytvorit voice assistenta na preklad karticek
+
+"""
