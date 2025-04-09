2b5084a39ceaf41d2cad9add45e8445a79329560
Tomas Svoboda <fredka171@gmail.com>
2025-04-09 23:00:22 +0200

    Implement random generator and update GUI.

--- window.py
+++ window.py
@@ -1,15 +1,18 @@
 import tkinter as tk
 import ctypes
-from english_words import ENGLISH_WORD
+from english_words import get_czech_word, get_english_word, get_random_number
 
 class Window:
     def __init__(self):
         self.root = tk.Tk()
         self.root.title("English Cards")
-        self.root.geometry("600x400")
+        self.root.geometry("800x200")
+        self.root.resizable(False, False)
+        self.random_number = get_random_number()
 
-        self.label = self.main_label(ENGLISH_WORD)
-        self.button = self.main_button("Next Word", self.get_word)
+        self.label = self.main_label(text='Vítej! Klikni Next pro start.')
+        self.trsl_button = self.translate_button("Translate", self.get_czech_word_label)
+        self.nxt_button = self.next_button("Next", self.get_english_word_label)
 
         self.button_state = tk.BooleanVar(value=False)
         self.voice_button = self.voice_button(self.get_voice_logo(), self.get_voice_click_on)
@@ -21,21 +24,31 @@ class Window:
 
     def main_label(self,text):
         label = tk.Label(self.root, text=text, font=("Helvetica",30), width=40)
-        label.pack(pady=50)
+        label.place(relx=0.5, rely=0.1, anchor="center")
         return label
 
-    def main_button(self, text, command):
-        button = tk.Button(self.root, text=text, command=command, font=("Helvetica", 20), width=10, height=5)
-        button.pack(pady=60)
+    def translate_button(self, text, command):
+        button = tk.Button(self.root, text=text, command=command, font=("Helvetica", 20), width=10, height=1)
+        button.place(relx=0.4, rely=0.5, anchor="center")
+        return button
+    
+    def next_button(self, text, command):
+        button = tk.Button(self.root, text=text, command=command, font=("Helvetica", 20), width=10, height=1)
+        button.place(relx=0.62, rely=0.5, anchor="center")
         return button
     
     def voice_button(self, text, command):
-        button = tk.Button(self.root, text=text, command=command, font=("Helvetica", 15), width=5, height=5)
-        button.place(relx=1.0, rely=1.0, anchor="se", x=-10, y=-10)
+        button = tk.Button(self.root, text=text, command=command, font=("Helvetica", 15), width=2, height=2)
+        button.place(relx=1.0, rely=1.0, anchor="se", x=-5, y=-5)
         return button
 
-    def get_word(self):
-        self.label.config(text="next word")
+    def get_czech_word_label(self):
+        self.label.config(text=get_czech_word(self.random_number))
+        self.random_number = get_random_number()
+
+    def get_english_word_label(self):
+        self.label.config(text=get_english_word(self.random_number))
+        
 
     def get_voice_logo(self):
         if self.button_state.get():
